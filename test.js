c666c5eb7b718ad0d062ea06650ce09fedf07b3d
rana.gamal@meti.dev <ranagamaldaif@gmail.com>
2025-07-21 23:35:49 +0300

    Add authentication function and capture additional screenshot

--- test.js
+++ test.js
@@ -5,5 +5,18 @@ const { chromium } = require('playwright');
   const page = await browser.newPage();
   await page.goto('https://bitheap.tech');
   await page.screenshot({ path: 'screenshot.png' });
-await browser.close();
+  await authenticate(page);
+  await browser.close();
 })();
+
+async function authenticate(page) {
+  await page.goto('https://bitheap.tech');
+  await page.click('#menu-item-2330');
+  await page.locator("[name='xoo-el-username']").fill(process.env.BITHEAP_USERNAME)
+  await page.locator("[name='xoo-el-password']").fill(process.env.PASS)
+  await page.locator('xpath=/html/body/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div/form/button').click()
+  const text = await page.locator('#menu-item-2333').textContent()
+  if(text != "Hello, Playwright")
+    console.error("The authentication was not successful!")
+  await page.screenshot({ path: 'screenshot2.png' });
+}
